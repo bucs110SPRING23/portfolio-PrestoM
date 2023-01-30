9865a2367b14ab691ec3dc9b304564795e7e3405
PrestoM <pmuirhead07@gmail.com>
2023-01-30 17:25:27 -0500

    exercise 1 complete

--- ch01/lab/main.py
+++ ch01/lab/main.py
@@ -1,1 +1,10 @@
+weeks = 16
+classes = 5
+tuition = 6000
+cost_per_week = ((tuition / classes) / weeks)
+classes_per_week = 3
+cost_per_class = cost_per_week/classes_per_week
+print("Cost per week:", cost_per_week)
+print("Cost per class:", cost_per_class)
 
+print()
