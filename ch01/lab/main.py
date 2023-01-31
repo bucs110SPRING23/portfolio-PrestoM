3f6a30bbd701b9efb0d1826cf6de07d744cada4e
PrestoM <pmuirhead07@gmail.com>
2023-01-30 20:46:19 -0500

    lab 1 complete

--- ch01/lab/main.py
+++ ch01/lab/main.py
@@ -1,3 +1,5 @@
+import random
+
 weeks = 16
 classes = 5
 tuition = 6000
@@ -7,4 +9,10 @@ cost_per_class = cost_per_week/classes_per_week
 print("Cost per week:", cost_per_week)
 print("Cost per class:", cost_per_class)
 
-print()
+vars = [weeks, classes, tuition, cost_per_week, classes_per_week, cost_per_class]
+for i in vars:
+    print(i, type(i))
+
+my_list = ["hi", 5, 1.0, 3.2, "4"]
+choice = random.choice(my_list)
+print(choice)
